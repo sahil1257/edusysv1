bbcf0bb21a8583aa6eff984a2a583cace90c04b2
SamiSahil <samisahil498@gmail.com>
2025-08-27 22:03:33 +0600

    s

--- Backend/server.js
+++ Backend/server.js
@@ -11,18 +11,23 @@ connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors({ origin: '*' })); // Allow all origins for developmentapp.use(express.json({ limit: '50mb' })); // To parse JSON request bodies
+
+app.use(cors({ origin: '*' })); // Allow all origins for development
+// ----------------------
+
+app.use(express.json({ limit: '50mb' })); // To parse JSON request bodies
 app.use(express.urlencoded({ extended: true, limit: '50mb' })); // To parse URL-encoded bodies
+
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'EduSys Pro API is online and running.Thanks For visiting......' });
 });
-// In server.js, near the other app.use() calls
+
 const path = require('path');
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const uploadRoutes = require('./routes/upload.routes.js');
 app.use('/api/upload', uploadRoutes);
+
 // --- API ROUTES ---
 app.use('/', require('./routes/auth.routes')); 
 app.use('/users', require('./routes/user.routes'));
